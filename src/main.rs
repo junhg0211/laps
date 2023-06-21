797a2d985c15a1fe66eb3fa8aedb67c7bf6e907d
Sch <junhg0211@gmail.com>
2023-06-21 20:19:26 +0900

    change event handling method

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use std::{
     fs::File,
     fs::create_dir_all,
     io::Write,
-    io::stdin,
     env::current_exe,
+};
+use inputbot::{
+    MouseButton::LeftButton,
+    handle_input_events,
 };
 
 fn lap() {
@@ -12,30 +15,33 @@ fn lap() {
     let now: DateTime<Local> = Local::now();
 
     // get file path
-    let path: String;
-
-    match current_exe() {
-        Ok(exe_path) => {
-            path = exe_path.parent().unwrap().display().to_string();
-        },
-        Err(_) => {return;}
-    }
-
-    let directory = format!("{}/{}", path, now.format("%Y%m%d"));
-    path = format!("{}/{}.txt", directory, now.format("%H%M%S%f"));
+    let directory = {
+        let folder_name = now.format("%Y%m%d").to_string();
+        current_exe().unwrap()
+            .parent().unwrap()
+            .join(folder_name)
+    };
+    let path = {
+        let filename = now.format("%H%M%S%f").to_string();
+        directory.join(filename)
+    };
 
     // format time
     let timestamp = format!("{}", now.format("%Y-%m-%d %H:%M:%S%.f"));
 
     // make file and write
     create_dir_all(directory).unwrap();
-    let mut file = File::create(path).unwrap();
+    let mut file = File::create(path.clone()).unwrap();
     file.write(&timestamp[..].as_bytes()).unwrap();
+
+    // log
+    println!("{}", path.to_str().unwrap());
 }
 
 fn main() {
-    loop {
-        stdin().read_line(&mut String::new()).unwrap();
-        lap();
-    }
+    // wait for left click
+    LeftButton.bind(|| lap());
+
+    // wait for input
+    handle_input_events();
 }
